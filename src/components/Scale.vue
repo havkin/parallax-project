652a6630c3e5277efc654025da3614a4036cd42f
Maxim Sozinov <max.sozinov@gmail.com>
2020-05-05 22:37:39 +0700

    scale done

--- src/components/Scale.vue
+++ src/components/Scale.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <h1>scale</h1>
 
     <div class="container">
       <svg
@@ -11,31 +10,22 @@
         <line 
           :x1="x0" 
           y1="80" 
-          :x2="xEnd" 
+          :x2="scaleLength" 
           y2="80" 
           style="stroke:rgb(0,0,0);stroke-width:2"/>
 
         <line 
-          v-for="line in smallMarksArr"
+          v-for="line in marksArr"
           :key="line.x"
           :x1="line.x" 
           y1="80" 
           :x2="line.x" 
-          y2="60" 
+          :y2="line.y" 
           style="stroke:rgb(0,0,0);stroke-width:1" />
 
-        <line 
-          v-for="(line, index) in marksArr"
-          :key="index+1"
-          :x1="line.x" 
-          y1="80" 
-          :x2="line.x" 
-          y2="30" 
-          style="stroke:rgb(0,0,0);stroke-width:2" />
-
         <text 
-          v-for="mark in signsArr"
-          :key="mark.value"
+          v-for="(mark, index) in signsArr"
+          :key="index"
           :x="mark.x" 
           y="95" 
           fill="black"
@@ -55,16 +45,17 @@ export default {
   props: {
     startDate: Number,
     endDate: Number,
-    step: Number
+    step: Number,
+    ratio: Number
   },
   data() {
     return {
       wpx: window.screen.availWidth,
-      x0: 0,
+      x0: 50,
       xEnd: 0,
       marksArr: [],
       signsArr: [],
-      smallMarksArr: [],
+      scaleLength: 0,
     }
   },
 
@@ -74,35 +65,52 @@ export default {
 
    methods: {
       calculateScale() {
-        const x0 = 0;
-        this.xEnd = Math.ceil(this.wpx * 1.3);
+
+        this.xEnd = Math.ceil(this.wpx * 1.3) - 50;
+        this.scaleLength = Math.ceil((this.xEnd - this.x0) * this.ratio)
+        
+
+
         const minMarkStep = Math.ceil(this.wpx / 10);
         
         let markQnt = Math.ceil((this.endDate - this.startDate) / this.step );
-        let markStep = Math.floor(this.xEnd / markQnt);
+        let markStep = Math.floor(this.scaleLength / markQnt);
 
         if (markStep < minMarkStep) {
           this.xEnd = minMarkStep * markQnt;
+          this.scaleLength = Math.ceil((this.xEnd - this.x0) * this.ratio)
           markStep = minMarkStep;
         }
 
         let sign = this.startDate;
-        for (let x = x0; x <= this.xEnd; x += markStep) {
-          this.marksArr.push({x})
-          this.signsArr.push({
-            x,
-            value: String(sign)})
-          sign += this.step
+        let bigMark = 12;
+        if (this.step !== 1) {
+          bigMark = this.step
         }
 
         let smallMarkQnt = 12 * markQnt;
         if (this.step !== 1) {
           smallMarkQnt = this.step * markQnt
         }
-        const smallMArkStep = Math.floor(this.xEnd / smallMarkQnt)
 
-        for (let x = x0; x <= this.xEnd; x += smallMArkStep) {
-          this.smallMarksArr.push({x})
+        const shift = Math.ceil((this.xEnd - this.x0) * (1 - this.ratio) / 2)
+        this.x0 += shift
+
+        const smallMArkStep = Math.floor(this.scaleLength / smallMarkQnt)
+        let counter = 0;
+        for (let x = this.x0; x <= this.scaleLength; x += smallMArkStep) {
+          if (counter % bigMark === 0) {
+            this.marksArr.push({x, y: 30})
+            this.signsArr.push({
+              x: x-17,
+              value: String(sign)})
+            sign += this.step
+          }
+          else {
+
+            this.marksArr.push({x, y: 60})
+          }
+          counter++
         }
       }
    }
@@ -114,18 +122,7 @@ export default {
 
 <style scoped>
 .container {
-  margin: 50px 100px;
+  /* margin: 50px 100px; */
 }
-.timeline {
-  position: relative;
-  display: flex;
-}
-.timeline-track {
-  position: absolute;
-  top: 5px;
-  width: 100%;
-  height: 2px;
-  background-color: #dfe3e4;
-  z-index: 1;
-}
+
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,30 +3,46 @@
     <div 
       class="relax"
       v-rellax="{
+      speed: 2,
+      vertical: false,
+      horizontal: true
+    }"
+    >
+      <scale 
+      :startDate="2010"
+      :endDate="2028"
+      :step="1"
+      :ratio="0.95"
+    />
+      
+    </div>
+    <div 
+      class="relax"
+      v-rellax="{
       speed: 3,
       vertical: false,
       horizontal: true
     }"
-    >I’m extra slow and smooth</div>
-    <div 
-      class="relax"
-      v-rellax="{
-      speed: 10,
-      vertical: false,
-      horizontal: true
-    }"
-    >I’m super fast!!</div>
-
-    <scale 
+    >
+      <scale 
       :startDate="2010"
       :endDate="2028"
       :step="1"
+      :ratio="1"
     />
+    </div>
+
+    <!-- <scale 
+      :startDate="2010"
+      :endDate="2028"
+      :step="1"
+    /> -->
     
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 import Scale from '@/components/Scale'
 
 export default {
@@ -34,10 +50,31 @@ export default {
   components: {
     Scale
   },
+
+  data() {
+    return {
+      scale: {}
+    }
+  },
+
+  created() {
+    this.getData()
+  },
+
   methods: {
     wheelHandler(event) {
       // console.log(event)
       event.view.scrollBy({left:event.deltaY, top:0, behavior: 'smooth'})
+    },
+
+    async getData() {
+      try {
+        const result = (await axios.get('/')).data
+        if (result) { 
+          this.scale = result
+        }
+      }
+      catch (e) { console.error(e) }
     }
   }
 }
@@ -51,11 +88,15 @@ export default {
   display: flex;
   flex-direction: column;
   justify-content: center;
+
 }
 
 .relax {
-  width: 3000px;
-  height: 100px;
-  border: 2px solid black;
+  display: flex;
+align-items: flex-end;
+
+  // width: 3000px;
+  height: 300px;
+  // border: 2px solid black;
 }
 </style>
